003ef5c50ae7ad1f06ab78a001b9d35794f99a80
Tyler <tydcarl@gmail.com>
2026-04-02 15:23:14 -0700

    1:17

--- lib/puter.hosting.ts
+++ lib/puter.hosting.ts
@@ -1,14 +1,29 @@
 import type { H } from "node_modules/react-router/dist/development/router-cLsU7kHk.mjs";
-import { HOSTING_CONFIG_KEY } from "./utils";
+import { createHostingSlug, HOSTING_CONFIG_KEY } from "./utils";
 import { puter } from "@heyputer/puter.js";
 
-type HostingConfig = { subdomain: string; }; 
+type HostingConfig = { subdomain: string };
 type HostedAsset = { url: string };
 
-export const getOrCreateHostingConfig = async (): Promise<HostingConfig | null> => {
-    const existing = (await puter.kv.get(HOSTING_CONFIG_KEY)) as HostingConfig | null; 
+export const getOrCreateHostingConfig =
+  async (): Promise<HostingConfig | null> => {
+    const existing = (await puter.kv.get(
+      HOSTING_CONFIG_KEY,
+    )) as HostingConfig | null;
 
-        if(existing?.subdomain) {subdomain: existing.subdomain };
+    if (existing?.subdomain)
+      return {
+        subdomain: existing.subdomain,
+      };
 
-        const subdomain = createHostingSlug();
-}
+    const subdomain = createHostingSlug();
+
+    try {
+      const created = await puter.hosting.create(subdomain, ".");
+
+      return { subdomain: created.subdomain };
+    } catch (e) {
+      console.warn(`Could not find subdomain: ${e}`);
+      return null;
+    }
+  };
