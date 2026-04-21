7462c2c1522add8fcc96134538143f208098ee71
Tyler <tydcarl@gmail.com>
2026-04-20 21:29:18 -0700

    1:31

--- lib/puter.action.ts
+++ lib/puter.action.ts
@@ -1,4 +1,10 @@
 import puter from "@heyputer/puter.js";
+import { get } from "http";
+import {
+  getOrCreateHostingConfig,
+  uploadImageToHosting,
+} from "./puter.hosting";
+import { isHostedUrl } from "./utils";
 
 export const signIn = async () => await puter.auth.signIn();
 
@@ -10,4 +16,46 @@ export const getCurrentUser = async () => {
   } catch {
     return null;
   }
+};
+
+export const createProject = async ({
+  item,
+}: CreateProjectParams): Promise<DesignItem | null | undefined> => {
+  const projectId = item.id;
+
+  const hosting = await getOrCreateHostingConfig();
+
+  const hostedSource = projectId
+    ? await uploadImageToHosting({
+        hosting,
+        url: item.sourceImage,
+        projectId,
+        label: "source",
+      })
+    : null;
+
+  const hostedRender =
+    projectId && item.renderedImage
+      ? await uploadImageToHosting({
+          hosting,
+          url: item.renderedImage,
+          projectId,
+          label: "rendered",
+        })
+      : null;
+
+  const resolvedSource =
+    hostedSource?.url ||
+    (isHostedUrl(item.sourceImage) ? item.sourceImage : "");
+
+  if (resolvedSource) {
+    console.warn("Failed to host souce image, skipping save.");
+    return null;
+  }
+
+  const resolvedRender = hostedRender?.url
+    ? hostedRender?.url
+    : item.renderedImage && isHostedUrl(item.renderedImage)
+      ? item.renderedImage
+      : undefined;
 };
