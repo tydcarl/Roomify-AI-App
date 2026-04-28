d0b190085033edec97d47120a2e3e94dc95e9b93
Tyler <tydcarl@gmail.com>
2026-04-28 14:39:08 -0700

    1:40

--- app/routes/Visualizer.$id.tsx
+++ app/routes/Visualizer.$id.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { useLocation } from "react-router";
 
 const VisualizerId = () => {
+  const location = useLocation();
+  const { initialImage, name } = location.state || {};
   return (
-    <div>VisualizerId</div>
-  )
-}
+    <section>
+      <h1>{name || "Untitled Project"}</h1>
 
-export default VisualizerId
+      <div className="visulaizer">
+        {initialImage && (
+          <div className="image_container">
+            <h2>Source Image</h2>
+            <img src={initialImage} alt="source" />
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default VisualizerId;
